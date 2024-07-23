de1ea074521129ae350bd99a489528e625c2023a
kingwilldabeast <willmur@live.com>
2024-07-23 16:24:37 -0500

    added header

---
+++ front-end-react/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Nav from './Nav'
+
+export default function Header (props) {
+
+    return (
+      
+      <div className="header">
+        <h1>Zapata's Pizza</h1>
+        <Nav/>
+      </div>
+    )
+  }
+
