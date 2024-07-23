32bfd12a9bd2a12f82932db41dfc82d2da9f5bfe
kingwilldabeast <willmur@live.com>
2024-07-23 16:24:19 -0500

    added file not found

---
+++ front-end-react/src/components/FileNotFound.jsx
@@ -0,0 +1,10 @@
+export default function FileNotFound (props) {
+    // console.log(props)
+    // const thing = ""
+    return (
+      
+      <div className="home">
+        <h2>Dough! Pizza not found</h2>
+      </div>
+    )
+  }
