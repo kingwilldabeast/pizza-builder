bfb2742b50d0464868e827430fa507f5a40a0867
kingwilldabeast <willmur@live.com>
2024-07-23 16:27:08 -0500

    created react files

---
+++ front-end-react/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import { Route, Routes, useNavigate, useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+import './App.css'
+
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
