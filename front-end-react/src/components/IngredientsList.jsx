44ee7420e06ac778ae9e4b12f948214ddf47e66d
kingwilldabeast <willmur@live.com>
2024-07-23 16:26:25 -0500

    added ingredients  index

---
+++ front-end-react/src/components/IngredientsList.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom"
+
+
+export default function IngredientsList () {
+
+    const [ingredients, setIngredients] = useState([])
+  
+    let navigate = useNavigate()
+    const showIngredient = (ingredientID) => {
+      navigate(`${ingredientID}`)
+    }
+
+    useEffect(()=>{
+      const getData = async () => {
+        const response = await axios.get(`http://127.0.0.1:8000/ingredients/`)
+        console.log('data', response)
+        
+        //assign API results to array
+        setIngredients(response.data)
+      }
+      getData()
+  
+    },[])
+
+    console.log(ingredients)
+    
+    if(!ingredients) {
+        return <h1>Loading... please wait</h1>
+    } else {
+        return (
+            <div className="objectList">
+    
+                {
+                    ingredients.map((ingredient, index) => (
+                        <div key={index} 
+                        className="objectItem"
+                        onClick={() => showIngredient(ingredient.id)}
+                        style={{
+                            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${ingredient.image_url})`, 
+                            backgroundSize:'600px', 
+                            backgroundPosition: 'center 80%'}} 
+                        >
+                            
+                            <h2>{ingredient.name}</h2>
+                            {/* <h3>Author: {ingredient.attributes.author}</h3> */}
+                            {/* <h3>Summary: {ingredient.attributes.summary}</h3> */}
+                        </div>    
+                    ))
+                }
+    
+            </div>
+        )
+
+    }
+
+  }
