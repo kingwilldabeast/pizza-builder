b9544549ed7948492a117d09a4acf407d2056a09
kingwilldabeast <willmur@live.com>
2024-07-23 16:25:46 -0500

    added location  show page

---
+++ front-end-react/src/components/LocationExpanded.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import {useParams, Link} from 'react-router-dom'
+import axios from 'axios'
+
+
+export default function LocationExpanded (props) {
+
+  const [location, setLocation] = useState('')
+
+  let {locationID} = useParams() 
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`http://127.0.0.1:8000/locations/${locationID}`)
+      console.log('data', response)
+      
+      //assign API results to array
+      setLocation(response.data)
+    }
+    getData()
+
+  }, [props.locations, locationID])
+
+  return location ? (
+    <div className="expandedItem">
+      <Link to ='/locations' className='navtext'>Back to locations</Link>
+        <br/>
+      <div className='expandedBody'>
+        <div className='expandedText'>
+        <h2>{location.name}</h2>
+
+
+        </div>
+
+        <img src = {location.image_url} 
+        className='expandedImage'
+        />
+      </div>
+    </div>
+  ) 
+  :     <div> 
+            <div className='home'>  
+            <h2>location not found</h2>
+            <Link to ='/locations' className='navtext'>Back to locations</Link>
+            </div> 
+        </div>
+}
+
